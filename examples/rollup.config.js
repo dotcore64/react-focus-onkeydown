88eb52e4221846aab32c903c66796ec4819286a2
Julian Grinblat <julian@dotcore.co.il>
2019-03-07 22:54:21 +0900

    Add back rollup-plugin-livereload

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -34,6 +35,7 @@ export default {
       contentBase: 'public',
       port: process.env.PORT || 3000,
     }),
+    livereload('public'),
   ],
   output: {
     file: './public/client.js',
